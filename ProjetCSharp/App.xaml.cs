cfc5a08e511fbe96f2958754dad410659ce6ef84
Base <bdonnay@helb-prigogine.be>
2023-02-07 18:31:54 +0100

    first commit baby

---
+++ ProjetCSharp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetCSharp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ProjetCSharp/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using ProjetCSharp.ViewModel;
+global using System.Diagnostics;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
