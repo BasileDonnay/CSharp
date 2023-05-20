b368c3d18462c849eff65cb26f2eca03d77dee44
Michel Sow <sowmichel@hotmail.com>
2023-05-20 16:45:04 +0200

    V2
    - Ajout du versioning,
    - On peu plus cree de course tant qu'on n'est pas connecter
    -Amelioration de la mainPage
    - Soucis avec le scanner car je ne peux pas simuler de port '' com '' ce qui signifie que je ne peux pas utiliser ma methode scanner

--- ProjectBase/ProjectBase/ViewModel/MainViewModel.cs
+++ ProjectBase/ProjectBase/ViewModel/MainViewModel.cs
@@ -1,142 +1,147 @@
-
 using System.Collections.ObjectModel;
 
-namespace ProjectBase.ViewModel;
-
-public partial class MainViewModel : BaseViewModel
+namespace ProjectBase.ViewModel
 {
-    [ObservableProperty]
-    string monTexte = " Go to details";
-    [ObservableProperty]
-    string activeTarget;
-    [ObservableProperty]
-    string monCode;
-
-
-    CourseService myService;
-    public ObservableCollection<User> ShownList { get; set; } = new();
-    public ObservableCollection<CourseModel> MyShownList { get; } = new();
-
-    DeviceOrientationServices MyDeviceOrientationService;
-    public MainViewModel(CourseService myService)
+    public partial class MainViewModel : BaseViewModel
     {
-        this.myService = myService;
+        // Propriétés observables
+        [ObservableProperty]
+        string monTexte = "Go to details";
+        [ObservableProperty]
+        string activeTarget;
+        [ObservableProperty]
+        string monCode;
 
-        this.MyDeviceOrientationService = new DeviceOrientationServices();
-        MyDeviceOrientationService.ConfigureScanner();
-        MyDeviceOrientationService.SerialBuffer.Changed += SerialBuffer_Changed;
+        CourseService myService;
+        public ObservableCollection<User> ShownList { get; set; } = new ObservableCollection<User>();
+        public ObservableCollection<CourseModel> MyShownList { get; } = new ObservableCollection<CourseModel>();
 
-        CreateUserTables MyUserTables = new();
-    }
+        DeviceOrientationServices MyDeviceOrientationService;
 
-    private void SerialBuffer_Changed(object sender, EventArgs e)
-    {
-
-        DeviceOrientationServices.QueueBuffer myQueue = (DeviceOrientationServices.QueueBuffer)sender;
-
-        //ID c'est la code QR que l'on va scanner
-        string codeQr = myQueue.Dequeue().ToString();
-
-        foreach (var data in Globals.MyList)
+        public MainViewModel(CourseService myService)
         {
-            //le .Code c'est un c'est attribut du course model, si c'est == alors je l'ajoute dans ma liste
-            if (codeQr == data.Code) 
-                MyShownList.Add(data);
-            //MonCode = codeQr;
-        }
-    }
+            this.myService = myService;
 
-    [RelayCommand]
-    async Task GoToDetailsPage(string data)
-    {
-        await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
-        {
-            {"Databc", data }
-        });
-    }
+            this.MyDeviceOrientationService = new DeviceOrientationServices();
+            MyDeviceOrientationService.ConfigureScanner();
+            MyDeviceOrientationService.SerialBuffer.Changed += SerialBuffer_Changed;
 
-
-    [RelayCommand]
-    async Task GoToRacePage(string courseCode)
-    {
-        await Shell.Current.GoToAsync(nameof(RacePage), true, new Dictionary<string, object>
-        {
-            {"MyCourseCode", courseCode }
-        });
-       
-        
-
-    }
-
-    [RelayCommand]
-    async Task GoToUserPage(string data)
-    {
-        await Shell.Current.GoToAsync(nameof(UserPage), true, new Dictionary<string, object>
-        {
-            {"Databc", data }
-        });
-    }
-
-    [RelayCommand]
-    async Task GoToLoginPage(string data)
-    {
-        await Shell.Current.GoToAsync(nameof(LoginPage), true, new Dictionary<string, object>
-        {
-            {"Databc", data }
-        });
-    }
-
-    public void RefreshPage()
-    {
-        MyShownList.Clear();
-
-        foreach (var item in Globals.MyList)
-        {
-            MyShownList.Add(item);
-            Debug.WriteLine(item.Nom);
+            CreateUserTables MyUserTables = new CreateUserTables();
         }
 
-    }
-
-    [RelayCommand]
-    async Task ShowCourses()
-    {
-        if (!Globals.isConnected)
+        private void SerialBuffer_Changed(object sender, EventArgs e)
         {
-            await Shell.Current.DisplayAlert("Database", "Not Connected", "OK");
-            return;
-        }
-        if (IsBusy) return;
-        CourseService MyService = new();
+            DeviceOrientationServices.QueueBuffer myQueue = (DeviceOrientationServices.QueueBuffer)sender;
 
-        bool test = MyDeviceOrientationService.mySerialPort.IsOpen;
-        try
+            // ID c'est le code QR que l'on va scanner
+            string codeQr = myQueue.Dequeue().ToString();
+
+            foreach (var data in Globals.MyList)
+            {
+                // Le .Code c'est un attribut du course model, si c'est == alors je l'ajoute dans ma liste
+                if (codeQr == data.Code)
+                    MyShownList.Add(data);
+                // MonCode = codeQr;
+            }
+        }
+
+        [RelayCommand]
+        async Task GoToDetailsPage(string data)
         {
-            IsBusy = true;
-            Globals.MyList = await MyService.GetCourse();
+            if (!Globals.isConnected)
+            {
+                await Shell.Current.DisplayAlert("Database", "Not Connected", "OK");
+                return;
+            }
+
+            await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
+            {
+                {"Databc", data }
+            });
         }
-        catch (Exception ex)
+
+        [RelayCommand]
+        async Task GoToRacePage(string courseCode)
         {
-            Debug.WriteLine($"Unable to get Students: {ex.Message}");
-            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+
+            await Shell.Current.GoToAsync(nameof(RacePage), true, new Dictionary<string, object>
+            {
+                {"MyCourseCode", courseCode }
+            });
         }
-        finally { IsBusy = false; }
 
-        MyShownList.Clear();
-
-
-
-
-        foreach (CourseModel stu in Globals.MyList)
+        [RelayCommand]
+        async Task GoToUserPage(string data)
         {
-            MyShownList.Add(stu);
+            await Shell.Current.GoToAsync(nameof(UserPage), true, new Dictionary<string, object>
+            {
+                {"Databc", data }
+            });
         }
-    }
-    [RelayCommand]
-    public async Task SetUsersJSONfile()
-    {
-        CourseService MyService = new();
 
-        myService.SetUsersJSONfile();
+        [RelayCommand]
+        async Task GoToLoginPage(string data)
+        {
+            await Shell.Current.GoToAsync(nameof(LoginPage), true, new Dictionary<string, object>
+            {
+                {"Databc", data }
+            });
+        }
+
+        public void RefreshPage()
+        {
+            MyShownList.Clear();
+
+            foreach (var item in Globals.MyList)
+            {
+                MyShownList.Add(item);
+                Debug.WriteLine(item.Nom);
+            }
+        }
+
+        [RelayCommand]
+        async Task ShowCourses()
+        {
+            if (!Globals.isConnected)
+            {
+                await Shell.Current.DisplayAlert("Database", "Not Connected", "OK");
+                return;
+            }
+
+            if (IsBusy) return;
+
+            CourseService MyService = new CourseService();
+
+            bool test = MyDeviceOrientationService.mySerialPort.IsOpen;
+            try
+            {
+                IsBusy = true;
+                Globals.MyList = await MyService.GetCourse();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get Students: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+            MyShownList.Clear();
+
+            foreach (CourseModel stu in Globals.MyList)
+            {
+                MyShownList.Add(stu);
+            }
+        }
+
+        [RelayCommand]
+        public async Task SetUsersJSONfile()
+        {
+            CourseService MyService = new CourseService();
+
+            myService.SetUsersJSONfile();
+        }
     }
 }
--- ProjectBase/ProjectBase/ViewModel/RaceViewModel.cs
+++ ProjectBase/ProjectBase/ViewModel/RaceViewModel.cs
@@ -1,73 +1,130 @@
-namespace ProjectBase.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using ProjectBase.Services;
 
-[QueryProperty(nameof(MonTxt), "Databc")]
-[QueryProperty(nameof(MaCourse), "MyCourseCode")]
-public partial class RaceViewModel : ObservableObject
+namespace ProjectBase.ViewModel
 {
-    [ObservableProperty]
-    int chronometre = 0;
-    [ObservableProperty]
-    String monTxt;
-
-    [ObservableProperty]
-    string maCourse;
-
-    [ObservableProperty]
-    string courseName;
-
-    [ObservableProperty]
-    string courseLocalite;
-
-    [ObservableProperty]
-    List<string> courseIndices;
-
-    private string courseIndicesAsString;
-
-    public string CourseIndicesAsString
+    // Décorateurs de propriétés pour la liaison de requête
+    [QueryProperty(nameof(MonTxt), "Databc")]
+    [QueryProperty(nameof(MaCourse), "MyCourseCode")]
+    public partial class RaceViewModel : ObservableObject
     {
-        get => courseIndicesAsString;
-        set => SetProperty(ref courseIndicesAsString, value);
-    }
+        DeviceOrientationServices MyDeviceOrientationService;
 
+        // Propriétés observables
+        [ObservableProperty]
+        int chronometre = 0;
+        [ObservableProperty]
+        string monTxt;
 
+        [ObservableProperty]
+        string maCourse;
 
-    public RaceViewModel()
-    {
-        var timer = Application.Current.Dispatcher.CreateTimer();
-        timer.Interval = TimeSpan.FromSeconds(1);
-        timer.Tick += (s, e) => UpdateTimer();
-        timer.Start();
-    }
+        [ObservableProperty]
+        string courseName;
 
+        [ObservableProperty]
+        string courseLocalite;
 
+        [ObservableProperty]
+        List<string> courseIndices;
 
-    partial void OnMaCourseChanged(string? value)
-    {
-        foreach (var course in Globals.MyList)
+        private string courseIndicesAsString;
+
+        public string CourseIndicesAsString
         {
-            if (course.Code == MaCourse)
-            {
-                CourseName = course.Nom;
-                CourseLocalite = course.Localite;
-                CourseIndices = course.Indices.Select(indice => indice.Name).ToList();
-                CourseIndicesAsString = string.Join(", ", CourseIndices);
+            get => courseIndicesAsString;
+            set => SetProperty(ref courseIndicesAsString, value);
+        }
 
-                break;
+        public RaceViewModel()
+        {
+            var timer = Application.Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) => UpdateTimer();
+            timer.Start();
+
+            /*
+            this.MyDeviceOrientationService = new DeviceOrientationServices();
+            try
+            {
+                // Essayer de configurer le scanner
+                MyDeviceOrientationService.ConfigureScanner();
+                MyDeviceOrientationService.mySerialPort.Open();
+                MyDeviceOrientationService.SerialBuffer.Changed += SerialBuffer_Changed;
+            }
+            catch (Exception)
+            {
+                // Si l'ouverture du port COM échoue, simuler un scan de code QR
+                SimulateQRScan("70");
+            }
+            */
+        }
+
+        /*
+        private void SerialBuffer_Changed(object sender, EventArgs e)
+        {
+            Console.WriteLine("salut");
+            DeviceOrientationServices.QueueBuffer myQueue = (DeviceOrientationServices.QueueBuffer)sender;
+
+            // ID c'est le code QR que l'on va scanner
+            string codeQr = myQueue.Dequeue().ToString();
+
+            foreach (var data in Globals.MyList)
+            {
+                // Le .Code c'est un attribut du course model, si c'est == alors je l'ajoute dans ma liste
+                if (codeQr == data.Code)
+                {
+                   
+                }
             }
         }
-    }
+        */
 
-
-
-
-
-    void UpdateTimer()
-    {
-        //MainThread.BeginInvokeOnMainThread();
-        MainThread.BeginInvokeOnMainThread(async () =>
+        /*
+        [RelayCommand]
+        public async Task SimulateQRScanCommand()
         {
-            //await Shell.Current.DisplayAlert("test", monTxt, "OK");
-            Chronometre += 1;
-        });
+            SimulateQRScan("70");
+        }
+
+        public void SimulateQRScan(string qrCode)
+        {
+            // Ajoutez le code QR à la file d'attente
+            MyDeviceOrientationService.SerialBuffer.Enqueue(qrCode);
+
+            // Déclenchez manuellement l'événement pour traiter le code QR
+            SerialBuffer_Changed(MyDeviceOrientationService.SerialBuffer, EventArgs.Empty);
+        }
+        */
+
+        partial void OnMaCourseChanged(string? value)
+        {
+            foreach (var course in Globals.MyList)
+            {
+                if (course.Code == MaCourse)
+                {
+                    CourseName = course.Nom;
+                    CourseLocalite = course.Localite;
+                    CourseIndices = course.Indices.Select(indice => indice.Name).ToList();
+                    CourseIndicesAsString = string.Join(", ", CourseIndices);
+
+                    break;
+                }
+            }
+        }
+
+        void UpdateTimer()
+        {
+            // MainThread.BeginInvokeOnMainThread();
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                //await Shell.Current.DisplayAlert("test", monTxt, "OK");
+                Chronometre += 1;
+            });
+        }
     }
 }
